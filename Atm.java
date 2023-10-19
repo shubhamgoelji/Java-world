28e743fb58240a1b30df634467668203cce28b55
Shubham Goel <131744608+shubhamgoelji@users.noreply.github.com>
2023-10-19 05:30:21 +0530

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -1,39 +1,63 @@
 import java.util.Scanner;
-public class Atm {
+
+public class Atm{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your choice ");
-        System.out.println("1.Balance enquiry");
-        System.out.println("2.Deposit");
-        System.out.println("3.Withdrawal");
-        int n=sc.nextInt();
-        switch(n)
-        {
-            case 1:
-        {
-                double balance=515.00;
-                System.out.println("Your account balance is "+balance);
-                break;
-        }
+         int balance = 5000, withdraw, deposit;
+    Scanner sc = new Scanner(System.in);
+ 
+    while (true)
+    {
+      System.out.println("ATM (Automated Teller Machine)");
+      System.out.println("Choose 1 for Withdraw");
+      System.out.println("Choose 2 for Deposit");
+      System.out.println("Choose 3 for Check Balance");
+      System.out.println("Choose 4 for EXIT");
+      System.out.print("Choose the operation you want to perform: ");
+ 
+      //user choice input 
+      int choice = sc.nextInt();
+      switch (choice)
+      {
+        case 1:
+          System.out.print("Enter the amount to be withdrawn: ");
+          withdraw = sc.nextInt();
+ 
+          //balance need to be with the withdrawn amount  
+          if (balance >= withdraw)  //for successful transaction 
+          {
+            balance = balance - withdraw;
+            System.out.println("Withdrawal successful! Please collect your money.");
+          }
+          else //not enough balance
+          {
+            System.out.println("Insufficient Balance");
+          }
+ 
+          System.out.println("");
+          break;
+ 
         case 2:
-        {
-            System.out.println("Enter the amount you want to deposit");
-            double a=sc.nextDouble();
-            double balance=515.00;
-            double newbalance=balance+a;
-            System.out.println(("Your current balance is"+newbalance));
-            break;
-        }
+          System.out.print("Enter amount to be deposited: ");
+          deposit = sc.nextInt();
+ 
+          //adding to the total balance 
+          balance = balance + deposit;
+          System.out.println("Your Money has been successfully depsited:");
+          System.out.println("");
+          break;
+ 
         case 3:
-        {
-            System.out.println("Enter the amount you want to withdrawal");
-            double b=sc.nextDouble();
-            double balance=515.00;
-            double newbalance=515.00-b;
-            System.out.println("Your new balance is "+newbalance);
-            break;
-        }
-        }
-        sc.close();
+          //balance check 
+          System.out.println("Available Balance: " + balance);
+          System.out.println("");
+          break;
+ 
+        case 4:
+          //for exit 
+          System.out.println("Successfully EXIT.");
+          System.exit(0);
+            }
+            
+        }   
     }
 }
